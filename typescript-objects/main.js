0290b612c15baa15238b690afe559ea9798c9e53
Owen Adams <falcocodes@osastack.dev>
2024-07-31 20:28:25 +0000

    complete typescript object exercise

--- typescript-objects/main.js
+++ typescript-objects/main.js
@@ -15,3 +15,22 @@ console.log(
 );
 console.log('value of student: ', student);
 console.log('typeof student: ', typeof student);
+const vehicle = {
+  make: 'vw',
+  model: 'passat',
+  year: 2008,
+};
+vehicle['color'] = 'black';
+vehicle['isConvertable'] = false;
+console.log("value of vehicle['color']: ", vehicle['color']);
+console.log("value of vehicle['isConvertable']: ", vehicle['isConvertable']);
+console.log('value of vehicle: ', vehicle);
+console.log('typeof vehicle: ', typeof vehicle);
+const pet = {
+  name: 'Todd',
+  kind: 'Toddcat',
+};
+delete pet.name;
+delete pet.kind;
+console.log('value of pet: ', pet);
+console.log('typeof pet: ', typeof pet);
--- typescript-objects/main.ts
+++ typescript-objects/main.ts
@@ -6,6 +6,19 @@ interface StudentProps {
   previousOccupation?: string;
 }
 
+type Vehicle = {
+  make: string;
+  model: string;
+  year: number;
+  color?: string;
+  isConvertable?: boolean;
+};
+
+interface Pet {
+  name?: string;
+  kind?: string;
+}
+
 const student: StudentProps = {
   firstName: 'Oweeoweeo',
   lastName: 'Lovemachine',
@@ -25,3 +38,28 @@ console.log(
 );
 console.log('value of student: ', student);
 console.log('typeof student: ', typeof student);
+
+const vehicle: Vehicle = {
+  make: 'vw',
+  model: 'passat',
+  year: 2008,
+};
+
+vehicle['color'] = 'black';
+vehicle['isConvertable'] = false;
+
+console.log("value of vehicle['color']: ", vehicle['color']);
+console.log("value of vehicle['isConvertable']: ", vehicle['isConvertable']);
+console.log('value of vehicle: ', vehicle);
+console.log('typeof vehicle: ', typeof vehicle);
+
+const pet: Pet = {
+  name: 'Todd',
+  kind: 'Toddcat',
+};
+
+delete pet.name;
+delete pet.kind;
+
+console.log('value of pet: ', pet);
+console.log('typeof pet: ', typeof pet);
