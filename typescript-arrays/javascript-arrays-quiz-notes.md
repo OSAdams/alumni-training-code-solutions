bc7954096c5fead1bb546ff281add460e26bd063
Owen Adams <falcocodes@osastack.dev>
2024-08-06 23:00:48 +0000

    complete exercise

--- typescript-arrays/javascript-arrays-quiz-notes.md
+++ typescript-arrays/javascript-arrays-quiz-notes.md
@@ -6,15 +6,31 @@ Answer the following questions in the provided markdown file before turning in t
 
 - What are arrays used for?
 
+  - Storing a list of `values` that are related to each other
+
 - Describe array literal notation.
+
+  - Values store inside square brackets `[]` and are separated by comas `,` assigned to a `variable`
+  - `const myArray = [true, 'string', function () { return 'hello woorrrlll' }, 1337, { name: 'dave' }]`
 
 - How are arrays different from "plain" objects?
 
+  - Objects have `key: 'value'` pairs
+  - Arrays have `indexes` and index `values`
+  - `const myArr: any[] = [1, 'string', false]` `myArr[1] === 'string'`
+
 - What number represents the first index of an array?
+
+  - 0
 
 - What is the `length` property of an array?
 
+  - the `length` property displays how many elements are in an array `[1, 2, 3]` has an`array.length === 3`
+
 - How do you calculate the last index of an array?
+  - `ourArray.length - 1`
+  - `[2, 3, 4]` last `index` would be `2`
+  - `[2, 3, 4].length - 1 === 2` `[2, 3, 4][2] === 4`
 
 ## Notes
 
--- typescript-arrays/main.js
+++ typescript-arrays/main.js
@@ -1,30 +1,18 @@
-const colors: string[] = ['red', 'white', 'blue'];
-
-for (const i in colors) {
+'use strict';
+const colors = ['red', 'white', 'blue'];
+for (let i in colors) {
   console.log(`colors[${i}]: ${colors[i]}`);
 }
-
 console.log(`America is ${colors[0]}, ${colors[1]}, ${colors[2]}`);
-
 colors[2] = 'green';
-
 console.log(`Mexico is ${colors[0]} ${colors[1]}, ${colors[2]}`);
-
 console.log('Value of colors: ', colors);
 console.log('typeof colors: ', typeof colors);
-
-const students: string[] = ['John', 'David', 'Micheal', 'Scott'];
-
-const numberOfStudents: number = students.length;
-
+const students = ['John', 'David', 'Micheal', 'Scott'];
+const numberOfStudents = students.length;
 console.log(`number of students: ${numberOfStudents}`);
-
-const lastIndex: number = numberOfStudents - 1;
-
-const lastStudent: string = students[lastIndex];
-
+const lastIndex = numberOfStudents - 1;
+const lastStudent = students[lastIndex];
 console.log(`last student: ${lastStudent}`);
-
 console.log('value of students: ', students);
-
 console.log('typeof students: ', typeof students);
