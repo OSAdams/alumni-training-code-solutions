9d94b6c81d3df3a07fc4b348ec78999808efe1a7
Owen Adams <falcocodes@osastack.dev>
2024-08-13 00:54:36 +0000

    complete exercise

--- typescript-conditionals/main.js
+++ typescript-conditionals/main.js
@@ -1,3 +1,4 @@
+'use strict';
 /* exported isUnderFive,
             isEven,
             startsWithJ,
@@ -8,18 +9,13 @@
             introduceWarnerBro,
             recommendMovie
  */
-
-const isUnderFive = (x: number): boolean => x < 5;
-const isEven = (x: number): boolean => x % 2 === 0;
-const startsWithJ = (string: string): boolean => string[0] === 'J';
-type Person = {
-  name: string;
-  age: number;
-};
-const isOldEnoughToDrink = (person: Person): boolean => person.age >= 21;
-const isOldEnoughToDrive = (person: Person): boolean => person.age >= 16;
-const isOldEnoughToDrinkAndDrive = (person: Person): boolean => person && false;
-const categorizeAcidity = (ph: number): string => {
+const isUnderFive = (x) => x < 5;
+const isEven = (x) => x % 2 === 0;
+const startsWithJ = (string) => string[0] === 'J';
+const isOldEnoughToDrink = (person) => person.age >= 21;
+const isOldEnoughToDrive = (person) => person.age >= 16;
+const isOldEnoughToDrinkAndDrive = (person) => false;
+const categorizeAcidity = (ph) => {
   if (ph >= 0 && ph < 7) {
     return 'acid';
   } else if (ph === 7) {
@@ -30,29 +26,36 @@ const categorizeAcidity = (ph: number): string => {
     return 'invalid pH level';
   }
 };
-const introduceWarnerBro = (name: string): string => {
+const introduceWarnerBro = (name) => {
   switch (name) {
     case 'wakko':
     case 'yakko':
       return "We're the warner brothers!";
+      break;
     case 'dot':
       return "I'm cute~";
+      break;
     default:
       return 'Goodnight everybody!';
   }
 };
-const recommendMovie = (genre: string): string => {
+const recommendMovie = (genre) => {
   switch (genre) {
     case 'action':
       return 'Die Hard';
+      break;
     case 'sci-fi':
       return 'Terrorizer';
+      break;
     case 'horror':
       return 'Terrorizer 2';
+      break;
     case 'musical':
       return 'Grease';
+      break;
     case 'drama':
       return 'Forest Gump';
+      break;
     default:
       return 'Genre not recognized. Choose between action, comedy, horror, drama, musical, or sci-fi';
   }
--- typescript-conditionals/typescript-conditionals-quiz-notes.md
+++ typescript-conditionals/typescript-conditionals-quiz-notes.md
@@ -6,21 +6,41 @@ Answer the following questions in the provided markdown file before turning in t
 
 - Give 6 examples of comparison operators.
 
+  - <, >, ==, ===, >=, <=, !==, !=
+
 - What data type do comparison expressions evaluate to?
+
+  - `boolean` value
 
 - What is the purpose of an `if` statement?
 
+  - to run a `{ code block }` _if_ a `expression` is met
+
 - Is `else` required in order to use an `if` statement?
+
+  - no goob
 
 - Describe the syntax (structure) of an `if` statement.
 
+  - `if` followed by a set of parenthesis `()`, inside the parenthesis an `expression` is set, followed by `{ code block }`
+
 - What are the three logical operators?
+
+  - logical or `||`
+  - logical and `&&`
+  - logical not `!`
 
 - How do you compare two different expressions in the same condition?
 
+  - Using a `logical` or, and, or not operators `||` `&&` `!`
+
 - What is the purpose of a `switch` statement?
 
+  - to execute code if a _condition_ of an `expression` is met
+
 - Is a `default` clause required in order to use a `switch` statement?
+
+  - no goob
 
 ## Notes
 
