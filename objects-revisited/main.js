c6b46f54098506710744ab81b70de5f995556438
Owen Adams <falcocodes@osastack.dev>
2024-09-11 18:52:40 +0000

    update to solution

--- objects-revisited/main.js
+++ objects-revisited/main.js
@@ -1,125 +1,102 @@
 'use strict';
-const weekDays = ['M', 'T', 'W', 'Th', 'F'];
-const weekends = ['S', 'Sa'];
-const positions = ['Nights Watch', 'Manager', 'Saiyan', 'Owner', 'Developer'];
-/*
-  Generate and return a random number at or between min and max number
-  In this example, we are going to set a min of 1 and a max of 3 to ensure
-  that our employees are working at most 3 work days to provide availability on weekends
-*/
-const generateNumberOfDaysWorking = (min, max) => {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  const numberOfDays = Math.floor(Math.random() * (max - min + 1)) + min;
-  return numberOfDays;
-};
-/*
-  Return an array of string values providing unique work weeks for each employee
-*/
-const daysWorkingArray = () => {
-  const daysWorking = [];
-  const limit = generateNumberOfDaysWorking(1, 3);
-  for (let i = 0; i < limit; i++) {
-    const currentDay = weekDays[Math.floor(Math.random() * weekDays.length)];
-    if (daysWorking.indexOf(currentDay) === -1) {
-      daysWorking.push(currentDay);
-    }
-  }
-  return daysWorking;
-};
 const business = {
   opens: '7am',
   closes: '5pm',
   totalEmployees: 3,
-  daysOpen: weekDays,
+  daysOpen: ['M', 'T', 'W', 'Th', 'F'],
   employees: {
     jon: {
-      position: 'Nights Watch',
-      daysOfWeekWorking: daysWorkingArray(),
+      position: 'Manager',
+      daysOfWeekWorking: ['M', 'T', 'W', 'Th', 'F'],
     },
     kakarot: {
-      position: 'Saiyan',
-      daysOfWeekWorking: daysWorkingArray(),
+      position: 'Security Officer',
+      daysOfWeekWorking: ['M', 'W', 'F'],
     },
     tori: {
       position: 'Owner',
-      daysOfWeekWorking: daysWorkingArray(),
+      daysOfWeekWorking: ['M', 'T', 'W', 'Th', 'F'],
     },
   },
 };
-/*
-  Add weekend workday to all employees (RIP) as well as updating the daysOpen array containing weekdays
-  We are going to make sure that 'S' and 'Sa' are not duplicating days in the daysOfWeekWorking array for
-  each employee as well as our daysOpen array for the business. Once we add more employees we should not have
-  any duplicate days for each of the named arrays
-*/
-const addWeekends = (days) => {
-  for (const day of days) {
-    if (day === 'S') {
-      if (business.daysOpen.indexOf(day) === -1) {
-        business.daysOpen.unshift(day);
-      }
-      for (const employee in business.employees) {
-        if (
-          business.employees[employee].daysOfWeekWorking.indexOf(day) === -1
-        ) {
-          business.employees[employee].daysOfWeekWorking.unshift(day);
-        }
-      }
-    } else {
-      if (business.daysOpen.indexOf(day) === -1) {
-        business.daysOpen.push(day);
-      }
-      for (const employee in business.employees) {
-        if (
-          business.employees[employee].daysOfWeekWorking.indexOf(day) === -1
-        ) {
-          business.employees[employee].daysOfWeekWorking.push(day);
-        }
-      }
-    }
+const addWeekends = () => {
+  business.daysOpen.unshift('Su');
+  business.daysOpen.push('S');
+  for (const employee in business.employees) {
+    const currentEmployee = business.employees[employee];
+    currentEmployee.daysOfWeekWorking.unshift('Su');
+    currentEmployee.daysOfWeekWorking.push('S');
   }
 };
-const addEmployees = (url, limit) => {
-  fetch(url)
-    .then((res) => res.json())
-    .then((result) => {
-      const newEmployees = result.slice(0, limit);
-      for (const employee of newEmployees) {
-        business.employees[employee.name] = {
-          position: positions[Math.floor(Math.random() * positions.length)],
-          daysOfWeekWorking: daysWorkingArray(),
-        };
-      }
-      business.totalEmployees += limit;
-      addWeekends(weekends);
-      checkIfFullTime();
-    })
-    .catch((error) => console.error({ error }));
-};
-const checkIfFullTime = () => {
-  for (const employee in business.employees) {
-    if (business.employees[employee].daysOfWeekWorking.length < 5) {
-      business.employees[employee].fullTime = false;
-    } else {
-      business.employees[employee].fullTime = true;
+const addEmployees = async () => {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
     }
+    const data = await response.json();
+    for (let i = 0; i <= 4; i++) {
+      const employeeFirstName = data[i].name.split(' ')[0].toLowerCase();
+      const newEmployeeObj = createEmployee();
+      business.employees[employeeFirstName] = newEmployeeObj;
+      business.totalEmployees++;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+const createEmployee = () => {
+  const positionsArr = [
+    'Developer',
+    'Designer',
+    'HR',
+    'Marketing',
+    'Customer Support',
+    'Receptionist',
+  ];
+  const randomPosition = Math.floor(Math.random() * positionsArr.length);
+  const employeePosition = positionsArr[randomPosition];
+  const randomDaysArr = generateRandomArr();
+  const newEmployee = {
+    position: employeePosition,
+    daysOfWeekWorking: ['Su', ...randomDaysArr, 'S'],
+  };
+  const isEmployeeFullTime = checkIfFullTime(newEmployee);
+  newEmployee.fullTime = isEmployeeFullTime;
+  return newEmployee;
+};
+const generateRandomArr = () => {
+  const daysArr = ['M', 'T', 'W', 'Th', 'F'];
+  const returnArr = [];
+  const randomLength = Math.floor(Math.random() * daysArr.length);
+  for (let i = 0; i <= randomLength; i++) {
+    const randomDay = Math.floor(Math.random() * daysArr.length);
+    if (!returnArr.includes(daysArr[randomDay])) {
+      returnArr.push(daysArr[randomDay]);
+    }
+  }
+  return returnArr;
+};
+const checkIfFullTime = (employee) => {
+  if (employee.daysOfWeekWorking.length >= 5) {
+    return true;
+  } else {
+    return false;
   }
 };
 const deleteEmployee = (employeeName) => {
-  for (const employee in business.employees) {
-    if (employee === employeeName) {
-      delete business.employees[employee];
-      business.totalEmployees--;
-    }
-  }
+  delete business.employees[employeeName];
+  business.totalEmployees--;
 };
 const updateObject = () => {
-  addWeekends(weekends);
-  checkIfFullTime();
-  addEmployees('https://jsonplaceholder.typicode.com/users', 4);
+  addWeekends();
+  addEmployees();
   deleteEmployee('jon');
-  console.log(business);
-  return business;
+  for (const employee in business.employees) {
+    const currentEmployee = business.employees[employee];
+    const isFullTime = checkIfFullTime(currentEmployee);
+    currentEmployee.fullTime = isFullTime;
+  }
+  console.log('business: ', business);
 };
-window.addEventListener('DOMContentLoaded', updateObject);
+document.addEventListener('DOMContentLoaded', updateObject);
--- objects-revisited/main.ts
+++ objects-revisited/main.ts
@@ -4,159 +4,127 @@ interface Employee {
   fullTime?: boolean;
 }
 
-interface Businesses {
+interface Business {
   opens: string;
   closes: string;
   totalEmployees: number;
   daysOpen: string[];
-  employees: {
-    [key: string]: Employee;
-  };
+  employees: Record<string, Employee>;
 }
 
-interface ApiEmployee {
+interface User {
+  id: number;
   name: string;
+  username: string;
+  email: string;
 }
 
-const weekDays: string[] = ['M', 'T', 'W', 'Th', 'F'];
-const weekends: string[] = ['S', 'Sa'];
-const positions: string[] = [
-  'Nights Watch',
-  'Manager',
-  'Saiyan',
-  'Owner',
-  'Developer',
-];
-
-/*
-  Generate and return a random number at or between min and max number
-  In this example, we are going to set a min of 1 and a max of 3 to ensure
-  that our employees are working at most 3 work days to provide availability on weekends
-*/
-
-const generateNumberOfDaysWorking = (min: number, max: number): number => {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  const numberOfDays = Math.floor(Math.random() * (max - min + 1)) + min;
-  return numberOfDays;
-};
-
-/*
-  Return an array of string values providing unique work weeks for each employee
-*/
-
-const daysWorkingArray = (): string[] => {
-  const daysWorking: string[] = [];
-  const limit = generateNumberOfDaysWorking(1, 3);
-  for (let i = 0; i < limit; i++) {
-    const currentDay = weekDays[Math.floor(Math.random() * weekDays.length)];
-    if (daysWorking.indexOf(currentDay) === -1) {
-      daysWorking.push(currentDay);
-    }
-  }
-  return daysWorking;
-};
-
-const business: Businesses = {
+const business: Business = {
   opens: '7am',
   closes: '5pm',
   totalEmployees: 3,
-  daysOpen: weekDays,
+  daysOpen: ['M', 'T', 'W', 'Th', 'F'],
   employees: {
     jon: {
-      position: 'Nights Watch',
-      daysOfWeekWorking: daysWorkingArray(),
+      position: 'Manager',
+      daysOfWeekWorking: ['M', 'T', 'W', 'Th', 'F'],
     },
     kakarot: {
-      position: 'Saiyan',
-      daysOfWeekWorking: daysWorkingArray(),
+      position: 'Security Officer',
+      daysOfWeekWorking: ['M', 'W', 'F'],
     },
     tori: {
       position: 'Owner',
-      daysOfWeekWorking: daysWorkingArray(),
+      daysOfWeekWorking: ['M', 'T', 'W', 'Th', 'F'],
     },
   },
 };
 
-/*
-  Add weekend workday to all employees (RIP) as well as updating the daysOpen array containing weekdays
-  We are going to make sure that 'S' and 'Sa' are not duplicating days in the daysOfWeekWorking array for
-  each employee as well as our daysOpen array for the business. Once we add more employees we should not have
-  any duplicate days for each of the named arrays
-*/
-
-const addWeekends = (days: string[]): void => {
-  for (const day of days) {
-    if (day === 'S') {
-      if (business.daysOpen.indexOf(day) === -1) {
-        business.daysOpen.unshift(day);
-      }
-      for (const employee in business.employees) {
-        if (
-          business.employees[employee].daysOfWeekWorking.indexOf(day) === -1
-        ) {
-          business.employees[employee].daysOfWeekWorking.unshift(day);
-        }
-      }
-    } else {
-      if (business.daysOpen.indexOf(day) === -1) {
-        business.daysOpen.push(day);
-      }
-      for (const employee in business.employees) {
-        if (
-          business.employees[employee].daysOfWeekWorking.indexOf(day) === -1
-        ) {
-          business.employees[employee].daysOfWeekWorking.push(day);
-        }
-      }
-    }
+const addWeekends = (): void => {
+  business.daysOpen.unshift('Su');
+  business.daysOpen.push('S');
+  for (const employee in business.employees) {
+    const currentEmployee = business.employees[employee];
+    currentEmployee.daysOfWeekWorking.unshift('Su');
+    currentEmployee.daysOfWeekWorking.push('S');
   }
 };
 
-const addEmployees = (url: string, limit: number): void => {
-  fetch(url)
-    .then((res) => res.json())
-    .then((result: ApiEmployee[]) => {
-      const newEmployees = result.slice(0, limit);
-      for (const employee of newEmployees) {
-        business.employees[employee.name] = {
-          position: positions[Math.floor(Math.random() * positions.length)],
-          daysOfWeekWorking: daysWorkingArray(),
-        };
-      }
-      business.totalEmployees += limit;
-      addWeekends(weekends);
-      checkIfFullTime();
-    })
-    .catch((error) => console.error({ error }));
+const addEmployees = async (): Promise<void> => {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const data = (await response.json()) as User[];
+    for (let i = 0; i <= 4; i++) {
+      const employeeFirstName = data[i].name.split(' ')[0].toLowerCase();
+      const newEmployeeObj = createEmployee();
+      business.employees[employeeFirstName] = newEmployeeObj;
+      business.totalEmployees++;
+    }
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-const checkIfFullTime = (): void => {
-  for (const employee in business.employees) {
-    if (business.employees[employee].daysOfWeekWorking.length < 5) {
-      business.employees[employee].fullTime = false;
-    } else {
-      business.employees[employee].fullTime = true;
+const createEmployee = (): Employee => {
+  const positionsArr = [
+    'Developer',
+    'Designer',
+    'HR',
+    'Marketing',
+    'Customer Support',
+    'Receptionist',
+  ];
+  const randomPosition = Math.floor(Math.random() * positionsArr.length);
+  const employeePosition = positionsArr[randomPosition];
+  const randomDaysArr = generateRandomArr();
+  const newEmployee: Employee = {
+    position: employeePosition,
+    daysOfWeekWorking: ['Su', ...randomDaysArr, 'S'],
+  };
+  const isEmployeeFullTime = checkIfFullTime(newEmployee);
+  newEmployee.fullTime = isEmployeeFullTime;
+  return newEmployee;
+};
+
+const generateRandomArr = (): string[] => {
+  const daysArr = ['M', 'T', 'W', 'Th', 'F'];
+  const returnArr: string[] = [];
+  const randomLength = Math.floor(Math.random() * daysArr.length);
+  for (let i = 0; i <= randomLength; i++) {
+    const randomDay = Math.floor(Math.random() * daysArr.length);
+    if (!returnArr.includes(daysArr[randomDay])) {
+      returnArr.push(daysArr[randomDay]);
     }
+  }
+  return returnArr;
+};
+
+const checkIfFullTime = (employee: Employee): boolean => {
+  if (employee.daysOfWeekWorking.length >= 5) {
+    return true;
+  } else {
+    return false;
   }
 };
 
 const deleteEmployee = (employeeName: string): void => {
-  for (const employee in business.employees) {
-    if (employee === employeeName) {
-      delete business.employees[employee];
-      business.totalEmployees--;
-    }
-  }
+  delete business.employees[employeeName];
+  business.totalEmployees--;
 };
 
-const updateObject = (): Businesses => {
-  addWeekends(weekends);
-  checkIfFullTime();
-  addEmployees('https://jsonplaceholder.typicode.com/users', 4);
+const updateObject = (): void => {
+  addWeekends();
+  addEmployees();
   deleteEmployee('jon');
-  console.log(business);
-  return business;
+  for (const employee in business.employees) {
+    const currentEmployee = business.employees[employee];
+    const isFullTime = checkIfFullTime(currentEmployee);
+    currentEmployee.fullTime = isFullTime;
+  }
+  console.log('business: ', business);
 };
 
-window.addEventListener('DOMContentLoaded', updateObject);
+document.addEventListener('DOMContentLoaded', updateObject);
