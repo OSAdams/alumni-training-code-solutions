d4b1b3fe82e4e3e309887684f8c22ce351f7d253
Owen Adams <falcocodes@osastack.dev>
2024-09-12 22:17:00 +0000

    update for-each callback

--- array-methods/for-each.ts
+++ array-methods/for-each.ts
@@ -1,11 +1,11 @@
 const values = [10, 1, 22, 23, 41, 5, 18, 7, 80, 9];
 
 console.log('In order:');
-values.forEach((element) => {
-  console.log(element);
+values.forEach((value) => {
+  console.log(value);
 });
 
 console.log('Reverse order:');
-values.forEach((element, index, array) => {
+values.forEach((value, index, array) => {
   console.log(array[array.length - 1 - index]);
 });
