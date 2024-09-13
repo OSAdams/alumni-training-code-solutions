b3b9536e809c09f448fc51c7ece4f0cb9af14473
Owen Adams <falcocodes@osastack.dev>
2024-09-13 17:18:00 +0000

    complete fetch exercise

---
+++ fetch/main.ts
@@ -0,0 +1,31 @@
+async function getUserData(): Promise<void> {
+  const url = 'https://jsonplaceholder.typicode.com/users';
+  try {
+    const response = await fetch(url, { method: 'GET' });
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+    const users = await response.json();
+    console.log(users);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+getUserData();
+
+async function getPokemonData(): Promise<void> {
+  const url = 'https://pokeapi.co/api/v2/pokemon/1';
+  try {
+    const response = await fetch(url, { method: 'GET' });
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+    const pokemon = await response.json();
+    console.log(pokemon);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+getPokemonData();
